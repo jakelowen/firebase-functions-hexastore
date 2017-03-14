4be9f5cbfbce50bfc2f301da98e98d72421e2b2d
Jake Lowen <jake@gpsimpact.com>
2017-03-14 11:17:45 -0500

    adding more comprehensive code comments into weavequery example

--- weaveQuery.js
+++ weaveQuery.js
@@ -10,21 +10,18 @@ var config = {
 	messagingSenderId: "999451438665"
 };
 firebase.initializeApp(config);
-
 var db = firebase.database()
 
+// Special syntax that includes FB path AND the set operation to perform 
+// | = or (deafault), & = INTERSECTION, - = difference
 var refPathsQuery = ["GRAPH/POS/LIKES/PYTHON/","|GRAPH/POS/LIKES/CAKE/","&GRAPH/POS/HATES/COCONUT/","-GRAPH/POS/RUNS/FAST/"]
+// will store the event listeners. useful for unbinding later
 var refs = []
-// var data = {
-// 	"|": [],
-// 	"&": [],
-// 	"-": [],
-// 	"entities":{},
-// 	"final":[]
-// }
+// container for the raw query results
 var queryComponentResults = {}
-
+// iterate over query components.
 _.map(refPathsQuery, pathQuery => {
+	// extract the path and set operation required
 	var path = null
 	var op = null
 	if ( ['|','&','-'].indexOf( pathQuery.charAt(0) ) !== -1 ) {
@@ -34,21 +31,29 @@ _.map(refPathsQuery, pathQuery => {
     	path = pathQuery.substr(0)
     	op = "|"
     }
-    var results = []
+    var results = [] // probably can eliminate in React
 	var listenPath = firebase.database().ref(path);
+	// Subscribe to changes on each path
 	var fn = listenPath.on('value', snapshot => {
 		fetched_data = snapshot.val()
 		queryComponentResults[pathQuery] = {op: op, data:fetched_data}
 		
+		// Wait until all queries have ran at least once 
+		// so we can peform set ops on complete data set
 		if (_.keys(queryComponentResults).length == refPathsQuery.length){
-			
 			// console.log("data is fully loaded")
 			var queryEntities = {}
 			var ops = {}
+			// iterate over the results of each set of results
 			_.map(_.keys(queryComponentResults), pq => {
+				// create one master lookup so we can easily lookup the timestamp of 
+				//each record involved in query
 				queryEntities = _.assign(queryEntities, queryComponentResults[pq].data)
+				// This simplifies the data structure for the set operations that are about to happen
+				// by grouping each kind of set operation together.
 				ops = _.assign(ops, {[queryComponentResults[pq].op]: _.keys(queryComponentResults[pq].data)})
 			})
+			// perform actual set operations to get results
 			var setOps = _.difference(
 				_.intersection(
 					ops["|"], 
@@ -56,14 +61,31 @@ _.map(refPathsQuery, pathQuery => {
 				), 
 				ops["-"]
 			)
+
+			// In react / redux, I probably need to do a .getstate to get the existing records in state 
+			// for comparison ops. Currently comparison ops are just done against the last
+			// record load.
+
+			// What records are new?
 			_.map(_.difference(setOps, results), key => {
+				// I think in React/Redux I will make these seperate Action calls that
+				// A) compare timestamp to record in store.
+				// B) add / remove records from store as necessary
+				// C) do .once queries to load records as necessary from id 
 				console.log("add", {[key]: queryEntities[key]})
 			})
+
+			// Which records are no longer in results?
 			_.map(_.difference(results,setOps), key => {
+				// I think in React/Redux I will make these seperate Action calls
+				// A) compare timestamp to record in store.
+				// B) add / remove records from store as necessary
+				// C) do .once queries to load records as necessary from id 
 				console.log("remove", {[key]: queryEntities[key]})
 			})
-			// console.log("Add new records", _.difference(setOps, results))
-			// console.log("Remove records", _.difference(results,setOps))
+
+			// Record final results for comparison on next iteration.
+			// IN react/REdux probably can eliminate this.
 			results = _.map(setOps, _.clone)
 
 			// console.log(queryComponentResults)
@@ -72,13 +94,6 @@ _.map(refPathsQuery, pathQuery => {
 			// console.log(setOps)
 			console.log("___________________________________________________")
 		}
-
-		
-
-		// data.final = results
-
-		// console.log(data)
-		// console.log("___________________________________________________")
 	})
 	refs.push(fn)
 });
